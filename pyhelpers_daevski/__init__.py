97799df5812038e59a8f6f6fa58aae159154602d
David McKee <daevski@gmail.com>
2023-06-28 11:37:25 -0400

    adding config writing (#3)
    Co-authored-by: David McKee <git@sarcasm.email>

--- pyhelpers_daevski/__init__.py
+++ pyhelpers_daevski/__init__.py
@@ -19,28 +19,35 @@ def get_configuration(config_file: Path):
         return yaml.safe_load(f)
 
 
+def set_configuration(configuration: dict, config_file: Path):
+    with config_file.open("w") as f:
+        f.write(yaml.safe_dump(configuration))
+
+
 def get_logger(
     appconfig: dict[Any, Any],
     logging_level: int = logging.INFO,
-    format: str = '[%Y-%m-%d] [%H:%M]',
-    configkey_logdir: str = 'LoggingDirectory',
+    format: str = "[%Y-%m-%d] [%H:%M]",
+    configkey_logdir: str = "LoggingDirectory",
 ):
     location = (
-        '.configy/logs' if appconfig[configkey_logdir] == 'default' else appconfig[configkey_logdir]
+        ".configy/logs"
+        if appconfig[configkey_logdir] == "default"
+        else appconfig[configkey_logdir]
     )
     Path(location).mkdir(parents=True, exist_ok=True)
-    timestamp = time.strftime('%Y-%m-%d_%H-%M')
+    timestamp = time.strftime("%Y-%m-%d_%H-%M")
     logging_file = Path(f"{location}") / f"{timestamp}.log"
     logging.basicConfig(
         level=logging_level,
         datefmt=format,
-        format='%(asctime)s %(levelname)s: %(message)s [PID: %(process)d]',
+        format="%(asctime)s %(levelname)s: %(message)s [PID: %(process)d]",
         handlers=[
             logging.FileHandler(logging_file),
             logging.StreamHandler(sys_stdout),
         ],
     )
-    logging.info('APP STARTUP')
+    logging.info("APP STARTUP")
     return logging
 
 
